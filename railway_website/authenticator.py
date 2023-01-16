4f6a3a37fcaad0216999e9bbbfb1f3969ebadb47
Marcel <marceltrzaskawka129@gmail.com>
2023-01-16 01:39:39 +0100

    Authenticator has colors now :3

--- railway_website/authenticator.py
+++ railway_website/authenticator.py
@@ -2,11 +2,12 @@ import hashlib
 import hmac
 import random
 import time
+from termcolor import colored
+import os
 
 random.seed('123')
 
-# key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
-key = 'F1SE4O60DXAY0AQSGPUH'
+
 hotp_counter = 0
 time_step_size = 30
 
@@ -31,6 +32,16 @@ def totp(key):
     return otp(key, str((int(time.time()) // time_step_size)))
 
 if __name__ == '__main__':
+
+    with open(os.path.join(os.path.dirname(os.path.relpath(__file__)), "private_key.txt")) as file:
+        key = file.readline()
+
+    if not key:
+        print("\n\033[31m!!!   WARNING   !!!\n\033[0m")
+        print("You have to first save your private key to the private_key.txt file")
+        print("It has to be in the same directory as the authenticator.py app\n")
+        exit()
+
     while True:
         print("Choose authenticator mode\n")
         print("HOTP \t\t[1]")
@@ -38,17 +49,17 @@ if __name__ == '__main__':
         print("OCRA \t\t[3]")
         print("Close app \t[4]\n")
         mode = input("Enter your choice: ")
-        print("====================\n")
+        print("\033[36m====================\n\033[0m")
 
         if mode == '1':
             while True:
                 print("Your HOTP: ", end='')
-                print(f"{hotp(key, hotp_counter):06d}")
+                print(f"\033[33m{hotp(key, hotp_counter):06d}\033[0m")
                 print("")
                 print("Generate another [1]")
                 print("Exit this mode   [2]\n")
                 choice = input("Enter your choice: ")
-                print("====================\n")
+                print("\033[36m====================\n\033[0m")
                 if choice == '1':
                     continue
                 elif choice == '2':
@@ -57,21 +68,21 @@ if __name__ == '__main__':
         elif mode == '2':
             while True:
                 remaining_time = time_step_size - int(time.time() % time_step_size)
-                print(f"\rTOTP: {totp(key):06d}\t Time Remaining: {remaining_time:02d}", end='')
+                print(f"\rTOTP: \033[33m{totp(key):06d}\033[0m\t Time Remaining: \033[32m{remaining_time:02d}\033[0m", end='')
                 if remaining_time == 1:
-                    print("\n====================\n")
+                    print("\033[36m\n\n====================\n\033[0m")
                     break
 
         elif mode == '3':
             while True:
                 challange = input("Please enter challange code: ")
-                print("====================\n")
-                print(f"OCRA: {otp(key, challange)}")
+                print("\033[36m\n====================\n\033[0m")
+                print(f"OCRA: \033[33m{otp(key, challange)}\033[0m")
                 print("")
                 print("Generate another [1]")
                 print("Exit this mode   [2]\n")
                 choice = input("Enter your choice: ")
-                print("====================\n")
+                print("\033[36m====================\n\033[0m")
                 if choice == '1':
                     continue
                 elif choice == '2':
@@ -80,4 +91,4 @@ if __name__ == '__main__':
         elif mode == '4':
             break
 
-        else: print("No, ale nie ten przycisk no\n")
+        else: print("\033[32mNo, ale nie ten przycisk no\n\033[0m")
